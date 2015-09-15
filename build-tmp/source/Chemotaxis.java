29eb779adf1b1b8fe1cb9c415ca81e6b7624f925
unknown <chdeming@697-334-Smaug.sf.k12.internal>
2015-09-15 10:32:07 -0700

    a lone bacteria moves across the screen

--- build-tmp/source/Chemotaxis.java
+++ build-tmp/source/Chemotaxis.java
@@ -27,15 +27,27 @@ public class Chemotaxis extends PApplet {
  public void draw()   
  {    
  	//move and show the bacteria 
- 	background(0, 0, 250);  
+ 	background(0, 0, 250);
+ 	joe.show();
+
  }  
  class Bacteria    
  {     
  	//lots of java! 
+ 	int x;
+ 	int y;
  	Bacteria()
  	{
+ 		y = width/2;
+ 		x = height/2;
 
  	}  
+ 	public void show()
+ 	{
+ 		ellipse(x, y, 25, 25);
+ 		y = y + (int)(Math.random()*5 - 2);
+ 		x = x + (int)(Math.random()*5 - 2);
+ 	}
  }    
   static public void main(String[] passedArgs) {
     String[] appletArgs = new String[] { "Chemotaxis" };
